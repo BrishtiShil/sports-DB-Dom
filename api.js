6c56d91b89a0e5e44dbb95933fb12d5832babdc0
Brishti Shil <brishtishil62@gmail.com>
2022-02-27 17:05:02 +0600

    fourth commit

--- api.js
+++ api.js
@@ -1,9 +1,58 @@
 const allPlayers = () => {
+    document.getElementById("player-container").innerHTML = "";
+    document.getAnimations("spinner").style.display = "block";
     const searchValue = document.getElementById("search-box").value;
     const url = `https://www.thesportsdb.com/api/v1/json/2/searchplayers.php?p=${searchValue}`;
-    console.log(url);
     fetch(url)
         .then(res => res.json())
-        .then(data => console.log(data));
-    console.log(searchValue);
-}
+        .then(data => showPlayer(data.player));
+};
+const showPlayer = (players) => {
+    if (players) {
+        document.getAnimations("spinner").style.display = "none";
+    }
+    else {
+        document.getAnimations("spinner").style.display = "block";
+
+    }
+    for (const player of players) {
+        const parent = document.getElementById("player-container");
+        const div = document.createElement("div");
+        div.innerHTML = `<div class="card border p-5">
+    <div class="pro-pic">
+        <img class="w-50" src="${player.strThumb}" alt="">
+    </div>
+    <h2>Name:${player.strPlayer}</h2>
+    <h5>Country:${player.strNationality}</h5>
+    <p></p>
+    <div class="allButton">
+        <button class="btn btn-danger">Delete</button>
+        <button onclick="details('${player.idPlayer}')" class="btn btn-success">Details</button>
+    </div>
+</div>`;
+        parent.appendChild(div);
+        // console.log(player);
+    }
+};
+const details = (id) => {
+    const url = `https://www.thesportsdb.com/api/v1/json/2/lookupplayer.php?id=${id}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => setDetail(data.players[0]));
+};
+const setDetail = (info) => {
+    if (info.strGender == "Male") {
+        document.getElementById("male").style.display = "block";
+        document.getElementById("female").style.display = "none";
+    }
+    else {
+        document.getElementById("male").style.display = "none";
+        document.getElementById("female").style.display = "block";
+    }
+
+    document.getElementById("details-container").innerHTML = `
+    <div>
+    <img src="" alt="">
+    <h1>Name: ${info.strPlayer}</h1>
+    </div>`;
+};
